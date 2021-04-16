b55763c476be6b41e3ba12841000d180cfa0b7d4
duxingzhe-w <wanghaitao5555@126.com>
2021-04-16 15:28:24 +0800

    Initial commit

---
+++ JAVA/src/com/company/Ex1_1.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        System.out.println("JAVA复习之路");
+
+    }
+}
---
+++ JAVA/src/test/demo/hello.java
@@ -0,0 +1,7 @@
+package test.demo;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello JAVA!");
+    }
+}
