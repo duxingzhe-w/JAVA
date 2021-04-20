6ed7e2b245aef304bed46f717099f2d425614fef
duxingzhe-w <wanghaitao5555@126.com>
2021-04-21 00:09:22 +0800

    添加文件-按钮事件实例

---
+++ JAVA/src/dxz/gui/Ex5_3.java
@@ -0,0 +1,39 @@
+package dxz.gui;
+//按钮事件实例
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Ex5_3 {
+    public static void main(String[] args) {
+        ButtonDemo myButtonGUI = new ButtonDemo();
+        myButtonGUI.setVisible(true);
+    }
+}
+class ButtonDemo extends JFrame implements ActionListener{
+    public static final int Width = 250;
+    public static final int Height = 200;
+    ButtonDemo() {
+        setSize(Width,Height);
+        setTitle("按钮事件");
+        Container conPane = getContentPane();
+        conPane.setBackground(Color.BLUE);
+        conPane.setLayout(new FlowLayout());
+        JButton redBut = new JButton("Red");
+        redBut.addActionListener(this);
+        conPane.add(redBut);
+        JButton greenBut = new JButton("Green");
+        greenBut.addActionListener(this);
+        conPane.add(greenBut);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Container conPane = getContentPane();
+        if (e.getActionCommand().equals("Red")) {
+            conPane.setBackground(Color.RED);
+        } else if (e.getActionCommand().equals("Green")) {
+            conPane.setBackground(Color.GREEN);
+        }
+    }
+}
